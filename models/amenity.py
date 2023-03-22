fc03a470d3ff3104751af12498711cced88cdecb
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-03-22 22:08:28 +0000

    Amenities

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+#from models.place import place_amenity
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = "amenities"
+    if (storage_type == "db"):
+        name = Column(String(128), nullable=False)
+        """
+        place_amenities = relationship("Place",
+                                       secondary=place_amenity,
+                                       back_populates="amenities")
+        """
+    else:
+        name = ""
