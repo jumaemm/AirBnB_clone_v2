b07d3b79b50f4959377ef44f7a96a2d526edf08d
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-04-09 14:23:40 +0000

    Setup script for static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,4 +4,21 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
-sudo ufw allow ""
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p "/data/web_static/releases/test/"
+sudo mkdir -p "/data/web_static/shared/"
+echo "<h1>Hello World</h1>" > /data/web_static/releases/test/index.html
+
+if [ -d "/data/web_static/current" ];
+then
+    sudo rm -rf /data/web_static/current;
+fi;
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
+
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+
+sudo service nginx restart
