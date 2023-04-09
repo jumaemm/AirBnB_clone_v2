1370d541bd5f4a3eca31be637aaef497792ec71c
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-04-09 21:14:04 +0000

    Authentication

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ Contains functions relating to deployment of web static files """
 
-from fabric.api import env, local, put, run
+from fabric.api import env, local, put, sudo
 from datetime import datetime
 from fabric.decorators import runs_once
 import os
 
-env.user = 'ubuntu'
+env.user = "ubuntu"
 env.hosts = ['18.210.10.94', '54.160.73.81']
+env.key_filename = "~/.ssh/id_rsa"
 
 
 @runs_once
@@ -32,21 +33,24 @@ def do_deploy(archive_path):
 
     file_name = os.path.basename(archive_path)
     folder = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format((folder_name)
+    folder_path = "/data/web_static/releases/{}/".format(folder)
     flag = False
 
     try:
+        print("Made it to try")
         put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, file_name))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
+        print("Put worked!")
+        sudo("mkdir -p {}".format(folder_path))
+        sudo("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        sudo("rm -rf /tmp/{}".format(file_name))
+        sudo("mv {}web_static/* {}".format(folder_path, file_name))
+        sudo("rm -rf {}web_static".format(folder_path))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {} /data/web_static/current".format(folder_path))
         print("New version deployed!")
         flag = True
 
     except Exception:
+        print("I am failing btw")
         flag = False
     return flag
